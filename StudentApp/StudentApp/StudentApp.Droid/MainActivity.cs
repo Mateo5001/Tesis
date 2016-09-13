84d4dda0e92d4fe4d9a095910d689d6bafd3b6ba
Daniel Miranda <danielomateo5001@hotmail.com>
2016-09-13 16:59:46 -0500

    cambios arreglo proyecto inciial

--- StudentApp/StudentApp/StudentApp.Droid/MainActivity.cs
+++ StudentApp/StudentApp/StudentApp.Droid/MainActivity.cs
@@ -16,6 +16,7 @@ using Android.Gms.Auth;
 using static Android.Gms.Common.Apis.GoogleApiClient;
 using Android.Gms.Auth.Api;
 using Android.Support.V7.App;
+using StudentApp.Views.MastePage;
 
 namespace StudentApp.Droid
 {
@@ -30,42 +31,7 @@ namespace StudentApp.Droid
     private static string TAG = "SignInActivity";
     private static int RC_SIGN_IN = 9001;
     #endregion
-
-    //private global::Xamarin.Forms.Button btnGoogleSingIn;
-    private bool intentInProgres;
-    private bool signInClicked;
-    private ConnectionResult mConnectionResult;
-
-
-
-    public void OnConnected(Bundle connectionHint)
-    {
-      signInClicked = true;
-      IPeople pe = PlusClass.PeopleApi;
-      IPerson persona = pe.GetCurrentPerson(mGoogleApiClient);
-      if (persona != null)
-      {
-        LoadApplication(new App());
-      }
-    }
-
-    public void OnConnectionFailed(ConnectionResult result)
-    {
-      if (!intentInProgres)
-      {
-        mConnectionResult = result;
-        if (signInClicked)
-        {
-          resolveSignInError();
-        }
-      }
-    }
-
-    public void OnConnectionSuspended(int cause)
-    {
-      throw new NotImplementedException();
-    }
-
+    
     protected override void OnCreate(Bundle bundle)
     {
       base.OnCreate(bundle);
@@ -86,63 +52,29 @@ namespace StudentApp.Droid
 
     private void InicioGPlusStart()
     {
-      #region Vieja forma de conexion 
-      //mGoogleApiClient = new GoogleApiClient.Builder(this)
-      // .AddConnectionCallbacks(this)
-      // .AddOnConnectionFailedListener(this)
-      // .AddApi(PlusClass.API)
-      // .AddScope(PlusClass.ScopePlusProfile)
-      // .AddScope(PlusClass.ScopePlusLogin)
-      // .Build();
-      #endregion
-
       mGoogleSigninOptions  = new GoogleSignInOptions.Builder(GoogleSignInOptions.DefaultSignIn)
         .RequestEmail()
         .RequestProfile()
         .Build();
-      
 
       mGoogleApiClient = new GoogleApiClient.Builder(this)
        .EnableAutoManage(this,this)
         .AddApi(Auth.GOOGLE_SIGN_IN_API, mGoogleSigninOptions)
         .Build();
-
-      mGoogleApiClient.Connect();
-
     }
 
     protected override void OnStart()
     {
       base.OnStart();
-      mGoogleApiClient.Connect();
     }
 
     protected override void OnStop()
     {
       base.OnStop();
-      if (mGoogleApiClient.IsConnected)
-      {
-        mGoogleApiClient.Disconnect();
-      }
     }
 
     protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
     {
-      #region anterior metodo
-      //if (requestCode == 0)
-      //{
-      //  if (resultCode != Result.Ok)
-      //  {
-      //    signInClicked = false;
-
-      //  }
-      //  intentInProgres = false;
-      //  if (!mGoogleApiClient.IsConnecting)
-      //  {
-      //    mGoogleApiClient.Connect();
-      //  }
-      //}
-      #endregion
       if (requestCode == RC_SIGN_IN)
       {
         GoogleSignInResult result = Auth.GoogleSignInApi.GetSignInResultFromIntent(data);
@@ -161,44 +93,19 @@ namespace StudentApp.Droid
       
     }
 
+    protected override void OnSaveInstanceState(Bundle outState)
+    {
+    }
+
     private void btnGoogleSingIn_Click(object sender, EventArgs e)
     {
-      #region forma anterior
-      //if (!mGoogleApiClient.IsConnecting)
-      //{
-      //  signInClicked = true;
-      //  mGoogleApiClient.Connect();
-      //  resolveSignInError();
-      //}
-      //if (!signInClicked && mGoogleApiClient.IsConnected)
-      //{
-      //  mGoogleApiClient.Disconnect();
-      //}
-      #endregion
-
       Intent signInIntent =Auth.GoogleSignInApi.GetSignInIntent(mGoogleApiClient);
       StartActivityForResult(signInIntent, RC_SIGN_IN);
     }
-    
-    private void resolveSignInError()
+
+    public void OnConnectionFailed(ConnectionResult result)
     {
-      if (mGoogleApiClient.IsConnected)
-      {
-        return;
-      }
-      if (mConnectionResult.HasResolution)
-      {
-        try
-        {
-          intentInProgres = true;
-          StartIntentSenderForResult(mConnectionResult.Resolution.IntentSender, 0, null, 0, 0, 0);
-        }
-        catch (Android.Content.IntentSender.SendIntentException e)
-        {
-          intentInProgres = false;
-          mGoogleApiClient.Connect();
-        }
-      }
+      throw new NotImplementedException();
     }
   }
 }
--- StudentApp/StudentApp/StudentApp.Droid/obj/Debug/android/src/md5f50ecbd0f3bbb2f8a30b5b8a843e9091/MainActivity.java
+++ StudentApp/StudentApp/StudentApp.Droid/obj/Debug/android/src/md5f50ecbd0f3bbb2f8a30b5b8a843e9091/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity
 			"n_onStart:()V:GetOnStartHandler\n" +
 			"n_onStop:()V:GetOnStopHandler\n" +
 			"n_onActivityResult:(IILandroid/content/Intent;)V:GetOnActivityResult_IILandroid_content_Intent_Handler\n" +
+			"n_onSaveInstanceState:(Landroid/os/Bundle;)V:GetOnSaveInstanceState_Landroid_os_Bundle_Handler\n" +
 			"n_onConnectionFailed:(Lcom/google/android/gms/common/ConnectionResult;)V:GetOnConnectionFailed_Lcom_google_android_gms_common_ConnectionResult_Handler:Android.Gms.Common.Apis.GoogleApiClient/IOnConnectionFailedListenerInvoker, Xamarin.GooglePlayServices.Basement\n" +
 			"";
 		mono.android.Runtime.register ("StudentApp.Droid.MainActivity, StudentApp.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MainActivity.class, __md_methods);
@@ -59,6 +60,14 @@ public class MainActivity
 	}
 
 	private native void n_onActivityResult (int p0, int p1, android.content.Intent p2);
+
+
+	public void onSaveInstanceState (android.os.Bundle p0)
+	{
+		n_onSaveInstanceState (p0);
+	}
+
+	private native void n_onSaveInstanceState (android.os.Bundle p0);
 
 
 	public void onConnectionFailed (com.google.android.gms.common.ConnectionResult p0)
