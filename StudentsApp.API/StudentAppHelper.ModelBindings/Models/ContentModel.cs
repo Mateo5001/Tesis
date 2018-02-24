d827506258bd078663f4a6a1092b155cd1047cd2
jar1802 <jar100_12@hotmail.com>
2018-02-24 14:43:36 -0500

    tag y content model

---
+++ StudentsApp.API/StudentAppHelper.ModelBindings/Models/ContentModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentAppHelper.ModelBindings.Models
+{
+   public class ContentModel
+  {
+    public int ContentTypeId;
+    public DateTime CreateDate;
+    public string ContentText;
+    public string FileUrl;
+    public int ContentUserId;
+  }
+}
